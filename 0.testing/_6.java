d48c9105226399c9764222c22b2e4e8ffc53dbf3
kxnyshk <ikanishk1172@gmail.com>
2022-03-24 23:38:12 +0530

    testing 5-6

---
+++ 0.testing/_6.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class _6 {
+    
+    public static void main(String[] args) {
+        
+        int[] Arr = {2,1,4,6,8,0,5};
+        Collections.reverse(Arrays.asList(Arr));
+        System.out.println(Arrays.toString(Arr));
+    }
+}
